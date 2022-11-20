929fd16b536af9f0d587762fbabbb34016e29fd5
gembalisrinu3 <gembalisrinu3@gmail.com>
2022-11-20 15:30:39 +0530

    Intial commit

---
+++ src/main/java/devops/com/Devops_Test_Training/DevopsTestTrainingApplication.java
@@ -0,0 +1,13 @@
+package devops.com.Devops_Test_Training;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DevopsTestTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DevopsTestTrainingApplication.class, args);
+	}
+
+}
---
+++ src/test/java/devops/com/Devops_Test_Training/DevopsTestTrainingApplicationTests.java
@@ -0,0 +1,13 @@
+package devops.com.Devops_Test_Training;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DevopsTestTrainingApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
